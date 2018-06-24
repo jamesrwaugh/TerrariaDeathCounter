f63a89e97224348c2b23432a96654ae59d7dbbbf
TehPwns <p00n3dj002@yahoo.com>
2018-06-23 22:09:12 -0400

    Initial integration of TShock APIs

--- Plugin.cs
+++ Plugin.cs
@@ -1,9 +1,12 @@
 using System;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.Localization;
 using TerrariaApi.Server;
 using TerrariaApi;
+using System.IO;
+using System.Diagnostics;
 
 namespace TerrariaDeathCounter
 {
@@ -38,7 +41,7 @@ namespace TerrariaDeathCounter
 		{
 			get
 			{
-				return "Records and reports player deaths from each source.";
+				return "Records and reports player deaths from each source, for laughs and stats.";
 			}
 		}
 
@@ -50,7 +53,65 @@ namespace TerrariaDeathCounter
 
 		public override void Initialize()
         {
-			
+            ServerApi.Hooks.NetGetData.Register(this, OnGetData);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.NetGetData.Deregister(this, OnGetData);
+            }
+            base.Dispose(disposing);
+        }
+
+        private void OnGetData(GetDataEventArgs e)
+        {
+            if (e.Handled)
+                return;
+
+            PacketTypes type = e.MsgID;
+            if (type != PacketTypes.PlayerDeathV2)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            Debug.WriteLine("Recv: {0:X}: {2} ({1:XX})", e.Msg.whoAmI, (byte)type, type);
+
+            using (MemoryStream data = new MemoryStream(e.Msg.readBuffer, e.Index, e.Length - 1))
+            {
+                // Exceptions are already handled
+                var player = Main.player[e.Msg.whoAmI];
+                e.Handled = RecordPlayerDeath(player, data);
+            }
+        }
+
+        private bool RecordPlayerDeath(Player player, MemoryStream data)
+        {
+            PlayerDeathReason playerDeathReason = PlayerDeathReason.FromReader(new BinaryReader(data));
+            Debug.WriteLine(string.Format("-> {0} -> {1}", GetNameOfKiller(playerDeathReason), playerDeathReason.GetDeathText(player.name)));
+            return true;
+        }
+
+        private string GetNameOfKiller(PlayerDeathReason reason)
+        {
+            if(reason.SourceNPCIndex != -1)
+            {
+                return Main.npc[reason.SourceNPCIndex].GetGivenOrTypeNetName()._text;
+            }
+            else if(reason.SourceProjectileType != -1)
+            {
+                return Lang.GetProjectileName(reason.SourceProjectileType).Key;
+            }
+            else if(reason.SourcePlayerIndex != -1)
+            {
+                return Main.player[reason.SourcePlayerIndex].name;
+            }
+            else
+            {
+                return "[Unkown Killer]";
+            }
         }
     }
 }
